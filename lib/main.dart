a0d8694d215fa7bb2ad3e834d6a767c6d6662487
GiorgiNadiradze <nadiradze19@gmail.com>
2022-11-18 16:38:23 +0400

    feature: add smart contract functions

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const VotingDapp());
+}
+
+class VotingDapp extends StatelessWidget {
+  const VotingDapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
