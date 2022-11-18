48da2feed87e90e9c89a1f48c5bd617ef471afe2
GiorgiNadiradze <nadiradze19@gmail.com>
2022-11-18 16:46:41 +0400

    feat: add home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:voting_dapp/page/home.dart';
 
-void main() {
-  runApp(const VotingDapp());
-}
+void main() => runApp(const VotingDapp());
 
 class VotingDapp extends StatelessWidget {
   const VotingDapp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const MaterialApp(
+      home: Home(),
+    );
   }
 }
